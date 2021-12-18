8e16285035b381dcbc58b049ca42a365725be8fb
Guilherme Nardari (NUC) <guinardari@usp.br>
2021-12-17 23:43:26 -0300

    including docker

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,9 @@ htop \
 guake \
 powerline \
 fonts-powerline \
+ca-certificates \
+gnupg \
+lsb-release \
 silversearcher-ag
 
 FILES=(
@@ -34,3 +37,15 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 
 mkdir $HOME/thirdparty
 git clone https://github.com/rupa/z.git $HOME/thirdparty/z
+
+# DOCKER
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+ sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io && sudo groupadd docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+# RUN THIS MANUALLY
+# sudo usermod -aG docker $USER && newgrp docker
